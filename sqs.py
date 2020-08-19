699afb5636bff2ce9c84c84d03f936b1031c7e38
Arkaprava Jana <arkaprava.j@gmail.com>
2020-08-19 14:53:57 +0530

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -4,7 +4,7 @@ import os
 def write_to_sqs(first_name,last_name):
     url = "http://169.254.169.254/latest/dynamic/instance-identity/document"
     res = requests.get(url)
-    region = res['region']
+    region = res.json()['region']
     client = boto3.client('sqs',region_name=region)
     data = {'firstname':first_name,'lastname':last_name}
     response = client.send_message( QueueUrl=os.environ['Q_URL'], MessageBody=str(data))
