ebcd29d14be3d71c9648d586f3a3b0d4195cf95b
Arkaprava Jana <arkaprava.j@gmail.com>
2020-08-21 18:24:10 +0530

    Update dynamo.py

--- dynamo.py
+++ dynamo.py
@@ -1,8 +1,12 @@
 import boto3
 import os
+import requests
 
 
 dynamo_table = os.environ['dynamo_table']
 def write_to_dynamo(first_name, last_name):
-    dynamodb = boto3.client('dynamodb')
+    url = "http://169.254.169.254/latest/dynamic/instance-identity/document"
+    res = requests.get(url)
+    region = res.json()['region']
+    dynamodb = boto3.client('dynamodb',region_name=region)
     dynamodb.put_item(TableName=dynamo_table, Item={'fuel_level':{'N':last_name},'vehicle_id':{'S':first_name}})
